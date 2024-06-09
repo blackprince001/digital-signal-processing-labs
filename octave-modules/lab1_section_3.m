c0dfaaa3fd6f9bcd4bdbe31df504ecd8cf27d49a
blackprince001 <appiahboaduprince@gmail.com>
2024-06-09 22:11:03 +0000

    fix we were generate graphs

--- octave-modules/lab1_section_3.m
+++ octave-modules/lab1_section_3.m
@@ -1,4 +1,5 @@
-Tp = 1/4000;
+f = 4000;
+Tp = 1/f;
 N_samples = 50;
 
 tt = linspace(-Tp, Tp, N_samples);
@@ -13,27 +14,27 @@ Amp2 = 1.2 * Amp1;
 tm1 = (37.2 / Month) * Tp;
 tm2 = -(41.3 / Day) * Tp;
 
-x1 = Amp1 * cos(2 * pi * 4000 * (tt - tm1));
-x2 = Amp2 * cos(2 * pi * 4000 * (tt - tm2));
+x1 = Amp1 * cos(2 * pi * f * (tt - tm1));
+x2 = Amp2 * cos(2 * pi * f * (tt - tm2));
 
 x3 = x1 + x2
 
 figure();
 
 subplot(3, 1, 1);
-plot(x1, tt);
+plot(tt, x1);
 xlabel("X1 Sinusoid");
 ylabel("tt range");
 title("X1 generated Signal");
 
 subplot(3, 1, 2);
-plot(x2, tt);
+plot(tt, x2);
 xlabel("X1 Sinusoid")
 ylabel("tt range");
 title("X2 generated Signal");
 
 subplot(3, 1, 3);
-plot(x3, tt);
+plot(tt, x3);
 xlabel("X1 Sinusoid")
 ylabel("tt range");
 title("X3 generated Signal");
